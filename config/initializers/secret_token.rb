328e25da8ef6ac98aadf8aa02fcab736a68ad5af
salterbw <brian.salter@witty-url.com>
2014-02-22 16:26:38 -0800

    Initial commit of base project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Saltosaurus::Application.config.secret_key_base = 'dda5860bc260f01007273ba1dceb58591a122e933f0b191f48989fe1fc89939bb14d4fcf35c5a7a5cbc053e839190cc6d6422e88d0637c6abc186811057b04b2'
