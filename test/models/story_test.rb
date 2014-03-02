13545d82873bad0bc349b4f34abf1fd4c5226a7b
salterbw <brian.salter@witty-url.com>
2014-03-01 23:55:53 -0800

    Adding Story model tests

--- test/models/story_test.rb
+++ test/models/story_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class StoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup()
+    @story = Story.new
+    story.name = 'ASD'
+    story.author = 'SAD'
+    story.story = 'asd'
+  end
+
+  test('valid with name, author and story') {
+    assert @story.valid?, 'Story has name, author and story but is invalid'
+  }
+
+  test('invalid without name') {
+    @story.name = nil
+    assert !@story.valid?, 'Story is missing name but is still valid'
+  }
+
+  test('invalid without author') {
+    @story.author = nil
+    assert !@story.valid?, 'Story is missing name but is still valid'
+  }
+
+  test('invalid without story') {
+    @story.story = nil
+    assert !@story.valid?, 'Story is missing name but is still valid'
+  }
+
+
+
 end
