f20d402ccc0f6308653b711a946fb5f0975864c6
salterbw <brian.salter@witty-url.com>
2014-03-02 19:32:27 -0800

    Adding comment unit tests

--- test/factories/comments.rb
+++ test/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :comment do
     sequence(:author) { |i| "Andrew#{i}" }
-    contents 'My back itches'
+    content 'My back itches'
     association :story
   end
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'valid with story, author and contents' do
+    comment = build(:comment)
+    assert comment.valid?, 'Comment has author, content and story but is invalid'
+  end
+
+  test 'invalid without story' do
+    comment = build(:comment, story: nil)
+    assert !comment.valid?, 'Story is missing author but is still valid'
+  end
+
+  test 'invalid without author' do
+    comment = build(:comment, author: nil)
+    assert !comment.valid?, 'Story is missing content but is still valid'
+  end
+
+  test 'invalid without content' do
+    comment = build(:comment, content: nil)
+    assert !comment.valid?, 'Story is missing story but is still valid'
+  end
+
 end
